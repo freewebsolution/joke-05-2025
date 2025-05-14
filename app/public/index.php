7d60c0cbfb15a67b46f501e095db07d528f5acc5
lucio ticali <luciano.ticali@technocenter.it>
2025-05-14 17:25:33 +0200

    Add Docker server inital configuration

--- app/public/index.php
+++ app/public/index.php
@@ -1,2 +1,8 @@
 <?php
-phpinfo();
+$pdo = new PDO('mysql:dbname=joke_db;host=mysql', 'root', 'root', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+$query = $pdo->query('SHOW VARIABLES like "version"');
+
+$row = $query->fetch();
+
+echo 'MySQL version:' . $row['Value'];
